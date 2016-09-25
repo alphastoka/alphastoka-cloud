3ac395aa5f76077c329ae45af9dd96b496310b3e
Pat Sabpisal <ssabpisa@me.com>
2016-09-25 21:32:38 +0700

    Ability to murder

--- alphastoka/alphastoka/urls.py
+++ alphastoka/alphastoka/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from mgmt import views
+from browse import views as browse_views
 from django.template.defaulttags import register
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^mgmt/create', views.mgmt_create),
-    url(r'^/?', views.index)
+    url(r'^mgmt/murder/([^\s]+)', views.mgmt_murder),
+    url(r'^/results', browse_views.index),
+    # default first view
+    url(r'^$', views.index)
 ]
 
 @register.filter
--- alphastoka/mgmt/views.py
+++ alphastoka/mgmt/views.py
@@ -3,6 +3,8 @@ from docker import Client
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 
+cli = Client(base_url='unix:///var/run/docker.sock')
+
 # Create your views here.
 def index(request):
 	cli = Client(base_url='unix:///var/run/docker.sock')
@@ -11,9 +13,18 @@ def index(request):
 		"containers": containers
 	})
 
+def results(request):
+	return render(request, 'results.html')
+
+def mgmt_murder(request, container_id):
+	# cli = Client(base_url='unix:///var/run/docker.sock')
+	cli.kill(container=container_id)
+	messages.add_message(request, 50, message='The selected Stoka has been taken care of.', extra_tags="success")
+	return HttpResponseRedirect("/") 
+
 def mgmt_create(request):
 	if request.method == "POST":
-		cli = Client(base_url='unix:///var/run/docker.sock')
+		# cli = Client(base_url='unix:///var/run/docker.sock')
 		dna = request.POST.get("dna")
 		group_name = request.POST.get("group_name")
 		seeder_username = request.POST.get("seeder_username")
@@ -29,6 +40,10 @@ def mgmt_create(request):
 		if dna is None:
 			messages.error(request, 'You did not specify a DNA for the Stoka instance.')
 			return HttpResponseRedirect("/") 
+		
+		if len(seeder_username) <= 2:
+			messages.add_message(request, 50, message='A seeder is required to spawn a Stoka Instance.', extra_tags="danger")
+			return HttpResponseRedirect("/") 
 
 		linkconf = cli.create_host_config(
 			links=[('rabbitmq','rabbitmqhost')]
@@ -36,6 +51,6 @@ def mgmt_create(request):
 
 		con = cli.create_container(image=dna, labels=lbl, environment=envs, host_config=linkconf)
 		response = cli.start(container=con.get("Id"))
-		print(response)
+		messages.add_message(request, 50, message='Stoka instance spawn successfully.', extra_tags="success")
 		
 	return HttpResponseRedirect("/") 
