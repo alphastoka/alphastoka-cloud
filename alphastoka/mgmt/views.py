564ef1b02e3c7f8f093dc61135125aeca4551c66
Pat Sabpisal <ssabpisa@me.com>
2016-09-26 23:18:24 +0700

    UI improve

--- alphastoka/mgmt/views.py
+++ alphastoka/mgmt/views.py
@@ -3,10 +3,30 @@ from docker import Client
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from pymongo import MongoClient
-import re
+import re, math
 
 cli = Client(base_url='unix:///var/run/docker.sock')
 mongo_client = MongoClient("mongodb://54.169.89.105:27017")
+
+def abbreviated_pages(n, page):
+
+	# Build set of pages to display
+	if n <= 10:
+		pages = set(range(1, n + 1))
+	else:
+		pages = (set(range(1, 6))
+		         | set(range(max(1, page - 2), min(page + 3, n + 1)))
+                 | set(range(n - 2, n + 1)))
+	
+	def display():
+		last_page = 0
+		for p in sorted(pages):
+			if p != last_page + 1: yield '...'
+			yield ('{0}' if p == page else '{0}').format(p)
+			last_page = p
+
+	return ' '.join(display()).split(' ')
+
 
 # Create your views here.
 def index(request):
@@ -37,20 +57,30 @@ def index(request):
 
 def results(request):
 	db = request.GET.get("family")
+	collections = ["instagram", "youtube", "facebook"]
+	collection = request.GET.get("collection", "instagram")
+	current_page = request.GET.get("page", "1")
 	if not db:
 		return render(request, "results.html", {
-			"dbs": mongo_client.database_names()
+			"dbs": mongo_client.database_names(),
+			"collections": collections
 		})
 	mongo_db = mongo_client[db]
 
-	count = mongo_db.human.count({})
-	humans = mongo_db.human.find({}).sort([("followed_by", -1)]).skip(0).limit(200)
+	count = mongo_db[collection].count({})
+	humans = mongo_db[collection].find({}).sort([("stats.subscriber_count", -1), ("followed_by", -1)]).skip(50*(int(current_page)-1)).limit(50)
+	
+	pages = abbreviated_pages(math.ceil(count/50),1)
 	
 	# self.mongo_db = self.mongo_client['stoka_' + ]
 	return render(request, "results.html", {
 		"family": db,
 		"count": count,
-		"humans": humans
+		"humans": humans,
+		"collections": collections,
+		"current_collection": collection,
+		"pages": pages,
+		"current_page" : current_page
 	})
 
 def mgmt_murder(request, container_id):
@@ -62,9 +92,9 @@ def mgmt_murder(request, container_id):
 def mgmt_create(request):
 	if request.method == "POST":
 		# cli = Client(base_url='unix:///var/run/docker.sock')
-		dna = request.POST.get("dna")
-		group_name = request.POST.get("group_name")
-		seeder_username = request.POST.get("seeder_username")
+		dna = request.POST.get("dna").replace(" ", "")
+		group_name = request.POST.get("group_name").replace(" ", "")
+		seeder_username = request.POST.get("seeder_username").replace(" ", "")
 		envs = {
 			"RABBIT_HOST": "rabbitmqhost",
 			"RABBIT_USR": "rabbitmq",
